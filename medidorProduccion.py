6c46ba876a34383eca19df18d5f3e9b8e924cb58
nachovera93 <69058627+nachovera93@users.noreply.github.com>
2022-06-08 17:50:13 -0400

    .

--- medidorProduccion.py
+++ medidorProduccion.py
@@ -72,8 +72,8 @@ print("Hora de comienzo:", horasetup)
 
 broker = '18.228.175.193'    #mqtt server
 port = 1883
-dId = '1234321'
-passw = 'EJsCZjuUIg'
+dId = '123432'
+passw = 'gS6hNbpsJC'
 webhook_endpoint = 'http://18.228.175.193:3001/api/getdevicecredentials'
 
 
