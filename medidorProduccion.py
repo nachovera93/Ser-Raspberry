556cfec4d9ed6da26156bafb0801dd0d4da87f74
nachovera93 <69058627+nachovera93@users.noreply.github.com>
2022-06-17 14:15:56 -0400

    .

--- medidorProduccion.py
+++ medidorProduccion.py
@@ -636,7 +636,7 @@ def Potencias(i,Irms,Vrms,potrmsCGE):
     TimeEnergy = datetime.datetime.now()
     if(TimeEnergy.minute==4):
         acceshourenergy=0
-    if(TimeEnergy.minute==19):
+    if(TimeEnergy.minute==29):
         if(acceshourenergy==0):
             print("Entrando a graficar")
             workbook=openpyxl.load_workbook(filename = dest_filename)
