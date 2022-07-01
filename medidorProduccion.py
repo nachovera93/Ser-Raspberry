8a0623f291a7a5be6261c5a867cc5789c51404f9
nachovera93 <69058627+nachovera93@users.noreply.github.com>
2022-07-01 13:55:41 -0400

    .

--- medidorProduccion.py
+++ medidorProduccion.py
@@ -637,7 +637,7 @@ def Potencias(i,Irms,Vrms,potrmsCGE):
     TimeEnergy = datetime.datetime.now()
     if(TimeEnergy.minute==4):
         acceshourenergy=0
-    if(TimeEnergy.minute==26):
+    if(TimeEnergy.minute==6):
         if(acceshourenergy==0):
             print("Entrando a graficar")
             workbook=openpyxl.load_workbook(filename = dest_filename)
@@ -651,7 +651,7 @@ def Potencias(i,Irms,Vrms,potrmsCGE):
             sheet20.append(list(dataHourFase1))
             sheet21.append(list(dataHourFase2))
             sheet22.append(list(dataHourFase3))
-            if(TimeEnergy.hour==13 and TimeEnergy.minute==26):
+            if(TimeEnergy.hour==14 and TimeEnergy.minute==6):
                 workbook=openpyxl.load_workbook(filename = dest_filename)
                 print("Entrando a GRAPH EXCEL")
                 sheet20 = workbook[f"MaxHora Fase 1 Diario"]
