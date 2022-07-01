d7417938e31ffc22a01f8819fdf4d5f5775b9f92
nachovera93 <69058627+nachovera93@users.noreply.github.com>
2022-07-01 19:41:22 -0400

    .

--- medidorProduccion.py
+++ medidorProduccion.py
@@ -637,7 +637,7 @@ def Potencias(i,Irms,Vrms,potrmsCGE):
     TimeEnergy = datetime.datetime.now()
     if(TimeEnergy.minute==4):
         acceshourenergy=0
-    if(TimeEnergy.minute==3):
+    if(TimeEnergy.minute=52):
         if(acceshourenergy==0):
             print("Entrando a graficar")
             workbook=openpyxl.load_workbook(filename = dest_filename)
@@ -651,7 +651,7 @@ def Potencias(i,Irms,Vrms,potrmsCGE):
             sheet20.append(list(dataHourFase1))
             sheet21.append(list(dataHourFase2))
             sheet22.append(list(dataHourFase3))
-            if(TimeEnergy.hour==0 and TimeEnergy.minute==3):
+            if(TimeEnergy.hour==19 and TimeEnergy.minute==52):
                 workbook=openpyxl.load_workbook(filename = dest_filename)
                 print("Entrando a GRAPH EXCEL")
                 sheet20 = workbook[f"MaxHora Fase 1 Diario"]
@@ -725,15 +725,13 @@ def Potencias(i,Irms,Vrms,potrmsCGE):
                 dia0=date.today()
                 dia=str(dia0-timedelta(1))
                 x=0
-                #for f in os.listdir('/home/pi/Desktop/Ser-Raspberry/'):
-                #    if(dia[5:7]==f[5:7]):
-                #        DiasExcel.append(f[5:7]) 
-                #        print(f)
-                #DiasExcel=DiasExcel.sort()
-                for f in DiasExcel:
+                for f in os.listdir('/home/pi/Desktop/Ser-Raspberry/'):
                     if(dia[5:7]==f[5:7]):
-                        x = x+1 
+                        DiasExcel.append(f) 
                         print(f)
+                DiasExcel=DiasExcel.sort(DiasExcel)
+                for f in DiasExcel:
+                        x = x+1 
                         try:
                             workbook=openpyxl.load_workbook(filename = f'{f}')
                             sheet1 = workbook[f"REDCompañia-Fase-1"]
