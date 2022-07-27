92b63a9024b0f5a70f73fa94ecc6987e7aa6584e
nachovera93 <69058627+nachovera93@users.noreply.github.com>
2022-07-27 17:09:16 -0400

    .

--- medidorProduccion.py
+++ medidorProduccion.py
@@ -640,7 +640,7 @@ def Potencias(i,Irms,Vrms,potrmsCGE):
     TimeEnergy = datetime.datetime.now()
     if(TimeEnergy.minute==4):
         acceshourenergy=0
-    if(TimeEnergy.minute==7):
+    if(TimeEnergy.minute==13):
         if(acceshourenergy==0):
             print("Entrando a graficar")
             workbook=openpyxl.load_workbook(filename = dest_filename)
@@ -664,7 +664,7 @@ def Potencias(i,Irms,Vrms,potrmsCGE):
             sheet21.append(list(dataHourFase2))
             sheet22.append(list(dataHourFase3))
             workbook.save(filename = dest_filename)
-            if(TimeEnergy.hour==16 and TimeEnergy.minute==7):
+            if(TimeEnergy.hour==16 and TimeEnergy.minute==13):
                 workbook=openpyxl.load_workbook(filename = dest_filename)
                 print("Entrando a GRAPH EXCEL")
                 sheet20 = workbook[f"MaxHora Fase 1 Diario"]
@@ -906,8 +906,9 @@ def Potencias(i,Irms,Vrms,potrmsCGE):
                 workbook.save(filename = dest_filename)
                 #excelcreate()
             print(f'Dest {dest_filename}')
+            timef=date.today()
             try:       
-                shutil.copy2(dest_filename, f'Respaldos/Respaldo-{dest_filename}')
+                shutil.copy2(dest_filename, f'Respaldos/Respaldo-{timef}.xlsx')
             except:
                 print("Continuar")
             #workbook.save(filename = dest_filename)
