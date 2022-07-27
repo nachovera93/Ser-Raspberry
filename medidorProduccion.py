edb800ad9093e1f5171b2040ed8581e7aa08135b
nachovera93 <69058627+nachovera93@users.noreply.github.com>
2022-07-27 16:12:37 -0400

    .

--- medidorProduccion.py
+++ medidorProduccion.py
@@ -640,7 +640,7 @@ def Potencias(i,Irms,Vrms,potrmsCGE):
     TimeEnergy = datetime.datetime.now()
     if(TimeEnergy.minute==4):
         acceshourenergy=0
-    if(TimeEnergy.minute==10):
+    if(TimeEnergy.minute==17):
         if(acceshourenergy==0):
             print("Entrando a graficar")
             workbook=openpyxl.load_workbook(filename = dest_filename)
@@ -664,7 +664,7 @@ def Potencias(i,Irms,Vrms,potrmsCGE):
             sheet21.append(list(dataHourFase2))
             sheet22.append(list(dataHourFase3))
             workbook.save(filename = dest_filename)
-            if(TimeEnergy.hour==16 and TimeEnergy.minute==10):
+            if(TimeEnergy.hour==16 and TimeEnergy.minute==17):
                 workbook=openpyxl.load_workbook(filename = dest_filename)
                 print("Entrando a GRAPH EXCEL")
                 sheet20 = workbook[f"MaxHora Fase 1 Diario"]
@@ -900,7 +900,7 @@ def Potencias(i,Irms,Vrms,potrmsCGE):
                 Energy_8=0
                 Energy_9=0
                 workbook.save(filename = dest_filename)
-                excelcreate()
+                #excelcreate()
             print(f'Dest {dest_filename}')
             try:       
                 shutil.copy2(dest_filename, f'Respaldos/Respaldo-{dest_filename}')
