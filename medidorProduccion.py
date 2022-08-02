3777c61c487403b405c35c68e7cd8cc949685ce1
nachovera93 <69058627+nachovera93@users.noreply.github.com>
2022-08-02 16:33:40 -0400

    .

--- medidorProduccion.py
+++ medidorProduccion.py
@@ -640,7 +640,7 @@ def Potencias(i,Irms,Vrms,potrmsCGE):
     TimeEnergy = datetime.datetime.now()
     if(TimeEnergy.minute==4):
         acceshourenergy=0
-    if(TimeEnergy.minute==3):
+    if(TimeEnergy.minute==40):
         if(acceshourenergy==0):
             print("Entrando a graficar")
             workbook=openpyxl.load_workbook(filename = dest_filename)
@@ -664,7 +664,7 @@ def Potencias(i,Irms,Vrms,potrmsCGE):
             sheet21.append(list(dataHourFase2))
             sheet22.append(list(dataHourFase3))
             workbook.save(filename = dest_filename)
-            if(TimeEnergy.hour==0 and TimeEnergy.minute==3):
+            if(TimeEnergy.hour==16 and TimeEnergy.minute==40):
                 workbook=openpyxl.load_workbook(filename = dest_filename)
                 print("Entrando a GRAPH EXCEL")
                 sheet20 = workbook[f"MaxHora Fase 1 Diario"]
@@ -4052,8 +4052,8 @@ def ReporteDiarioDia(datetim,Energy_RedCompañia,Energy_Paneles,Energy_Carga):
         array2 = np.array([[round(Energy_Paneles,5)]])
         array3 = np.array([[round(Energy_Carga,5)]]) 
         array4 = np.array([[round(val,1)]]) 
-        datetim=json.dumps(datetim, default=str)
-        worksheet.update(f'A{Largo+1}',datetim)
+        array5=np.array(str(array5))
+        worksheet.update(f'A{Largo+1}',array5.tolist())
         worksheet.update(f'B{Largo+1}', array.tolist())
         worksheet.update(f'C{Largo+1}', array2.tolist())
         worksheet.update(f'D{Largo+1}', array3.tolist())
